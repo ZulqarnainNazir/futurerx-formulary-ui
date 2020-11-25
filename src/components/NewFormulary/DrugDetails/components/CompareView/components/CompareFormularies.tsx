ff151ea64a7cd753af9048e4fa229eba6937dc64
umair-alii <umairsajjadcs@gmail.com>
2020-11-25 19:41:57 +0500

    formulary screen header

---
+++ src/components/NewFormulary/DrugDetails/components/CompareView/components/CompareFormularies.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CompareView.scss';
+// import vector from "../../../../../../assets/img/Vector.png";
+
+export default class CompareFormularies extends React.Component<any,any>{
+    render(){
+        return (
+            <div className="compare-formularies-container">
+                <h6>Select formularies for comparison</h6>
+            </div>
+        )
+    }
+}
