c0ac935d1b10b7ddb787c1d4a11840d8682ebbfc
Shahzaib Shahid <shahzaib.shahid912@gmail.com>
2020-11-29 19:37:50 +0500

    - added medicare information section - extended styles for panel header to support required and className props

--- src/components/NewFormulary/DrugDetails/components/FormularySetUp/FormularySetUp.tsx
+++ src/components/NewFormulary/DrugDetails/components/FormularySetUp/FormularySetUp.tsx
@@ -3,16 +3,17 @@ import "./FormularySetUp.scss";
 import GeneralInformation from "./components/GeneralInformation";
 import FormularyDesign from "./components/FormularyDesign";
 import FormularyTiers from "./components/FormularyTiers";
+import MedicareInformation from "./components/MedicareInformation";
 
 export default class FormularySetUp extends React.Component<any, any> {
   render() {
     return(
       <div>
         <GeneralInformation />
+        <MedicareInformation/>
         <FormularyDesign />
         <FormularyTiers />
       </div>
     );
-     
   };
 }
