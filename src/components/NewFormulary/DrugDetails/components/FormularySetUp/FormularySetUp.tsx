76620af680cd01fb68645f073f6d85777956964c
Shahzaib Shahid <shahzaib.shahid912@gmail.com>
2020-11-29 20:49:27 +0500

    - developed cormulary design section - changed the forumalry deisgn component to supllemental models

--- src/components/NewFormulary/DrugDetails/components/FormularySetUp/FormularySetUp.tsx
+++ src/components/NewFormulary/DrugDetails/components/FormularySetUp/FormularySetUp.tsx
@@ -4,7 +4,9 @@ import GeneralInformation from "./components/GeneralInformation";
 import FormularyDesign from "./components/FormularyDesign";
 import FormularyTiers from "./components/FormularyTiers";
 import MedicareInformation from "./components/MedicareInformation";
-
+import SupplementalModels from "./components/SupplementalModels";
+import Box from '@material-ui/core/Box';
+import Button from '../../../../shared/Frx-components/button/Button';
 export default class FormularySetUp extends React.Component<any, any> {
   render() {
     return(
@@ -13,6 +15,15 @@ export default class FormularySetUp extends React.Component<any, any> {
         <MedicareInformation/>
         <FormularyDesign />
         <FormularyTiers />
+        <SupplementalModels/>
+        <div className="btn-action">
+          <Box display="flex" justifyContent="flex-end" className="save-btn">
+            <Button label="Save" />
+          </Box>
+          <Box display="flex" justifyContent="flex-end" className="save-and-continue-btn">
+            <Button label="Save & Continue" />
+          </Box>
+        </div>
       </div>
     );
   };
