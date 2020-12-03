7e824a121c35bc9c9bbb4decedf431876fbc090e
Dhaval Vadher <vadher.dhaval@gmail.com>
2020-12-04 00:47:00 +0530

    add new table

--- src/components/NewFormulary/DrugDetails/components/FormularyConfigure/components/TierRemove.tsx
+++ src/components/NewFormulary/DrugDetails/components/FormularyConfigure/components/TierRemove.tsx
@@ -1,23 +1,51 @@
 import React from "react";
 
 import "./Tier.scss";
-
+import {Table} from 'antd';
 import FrxDrugGridContainer from "../../../../../shared/FrxGrid/FrxDrugGridContainer";
 import { tierRemoveColumns } from "../../../../../../utils/grid/columns";
 import { TierRemoveMockData } from "../../../../../../mocks/TierMock";
 
 class TierRemove extends React.Component {
   render() {
+    const dataSource = [
+      {
+        key: '1',
+        tierName: 'Tier 0',
+      },
+      {
+        key: '2',
+        tierName: 'Tier 1',
+      },
+    ];
+    
+    const columns = [
+      {
+        title: 'Tier Name',
+        dataIndex: 'tierName',
+        key: 'tierName',
+      },
+      
+    ];
     return (
       <>
         <div className="tier-grid-remove-container">
+          <Table 
+            columns={columns} 
+            dataSource={dataSource}
+            pagination={false}
+            rowSelection={{
+              columnWidth: 20,
+              fixed: true,
+              type: "checkbox",
+            }} />
           <FrxDrugGridContainer
             hideClearFilter
             isPinningEnabled={false}
             enableSearch={false}
             enableColumnDrag={false}
             onSearch={() => {}}
-            fixedColumnKeys={[]}
+            fixedColumnKeys={["tierName"]}
             pagintionPosition="topRight"
             gridName="TIER REMOVE"
             enableSettings={false}
