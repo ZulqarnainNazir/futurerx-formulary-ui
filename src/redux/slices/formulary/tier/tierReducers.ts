ab787bde1fb4cdee6f268dff3b0ee7afaddc5251
hanehosur <hanehosur@futurerx.com>
2020-12-15 18:52:51 +0530

    Add new functionality for tier

--- src/redux/slices/formulary/tier/tierReducers.ts
+++ src/redux/slices/formulary/tier/tierReducers.ts
@@ -22,6 +22,27 @@ export const getTierRejected = (state, action) => {
 
 }
 
+export const getTierLabelFulfilled = (state, action) => {
+  console.log("Reducer::getTierLabelFulfilled");
+  state.isLoading = false;
+  console.log(action)
+  if(action.payload.data === undefined || !Array.isArray(action.payload.data) || (action.payload.data.length === 0)) {
+    console.log("getTierLabelFulfilled: Payload invalid");
+    return;
+  }
+  const data = action.payload.data;
+  // Response stored in the redux store.
+  state.tierLabels = data;
+  
+}
+
+export const getTierLabelRejected = (state, action) => {
+  console.log("Reducer::getTierLabelRejected");
+  state.isLoading = false;
+  state.tierLabels = {};
+
+}
+
 export const postTierApplyFulfilled = (state, action) => {
   console.log("Reducer::postTierApplyFulfilled");
   state.isLoading = false;
@@ -40,5 +61,26 @@ export const postTierApplyRejected = (state, action) => {
   console.log("Reducer::postTierApplyRejected");
   state.isLoading = false;
   state.applyData = {};
+
+}
+
+export const postTierNewFulfilled = (state, action) => {
+  console.log("Reducer::postTierNewFulfilled");
+  state.isLoading = false;
+  console.log(action)
+  if(action.payload) {
+    console.log("postTierNewFulfilled: Payload invalid");
+    return;
+  }
+  const data = action.payload;
+  // Response stored in the redux store.
+  state.tierAddNewResponse = data;
+  
+}
+
+export const postTierNewRejected = (state, action) => {
+  console.log("Reducer::postTierNewRejected");
+  state.isLoading = false;
+  state.tierAddNewResponse = {};
 
 }
