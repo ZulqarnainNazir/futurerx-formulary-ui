9b1303a0b2ac97d251f704d4d7cfd946abacefa1
supraj <ssubbanna@futurerx.com>
2020-12-16 19:36:38 +0530

    Common method fixes to Drug Details

--- src/api/http-drug-details.ts
+++ src/api/http-drug-details.ts
@@ -1,3 +1,5 @@
+import { BASE_URL1 } from "./http-helper";
+
 export const GET_DRUG_SUMMARY_LA = "api/1/formulary-drug-summary-la/";
 export const GET_LA_FORMULARY_DRUGS = "api/1/formulary-drugs-la/";
 export const APPLY_LA_DRUG = "api/1/apply-formulary-drug-la/";
@@ -8,3 +10,39 @@ export const KEY_LIMIT = "limit";
 
 export const TYPE_REPLACE = "replace";
 export const TYPE_REMOVE = "remove";
+
+const BEARER_KEY = "b8625daa-ccbd-4167-8cde-673de141fd11";
+
+export const buildUrl = ({ refUrl = BASE_URL1, apiDetails }) => {
+  let apiPart = apiDetails.apiPart;
+  let pathParams = apiDetails.pathParams;
+  let keyVals = apiDetails.keyVals;
+  let url = refUrl + apiPart + pathParams;
+  if (keyVals) {
+    keyVals = keyVals.map((pair) => pair.key + "=" + pair.value);
+    url = url + "?" + keyVals.join("&");
+  }
+  return url;
+};
+
+export const getHeaders = () => {
+  return {
+    headers: {
+      Authorization: `Bearer ${BEARER_KEY}`,
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  };
+};
+
+export const postHeaders = (apiDetails) => {
+  return {
+    method: "POST",
+    body: JSON.stringify(apiDetails?.messageBody),
+    headers: {
+      Authorization: `Bearer ${BEARER_KEY}`,
+      Accept: "application/json",
+      "Content-Type": "application/json;charset=UTF-8",
+    },
+  };
+};
--- src/redux/slices/formulary/drugDetails/drugDetailLA/drugDetailLAActionCreation.ts
+++ src/redux/slices/formulary/drugDetails/drugDetailLA/drugDetailLAActionCreation.ts
@@ -1,5 +1,9 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { BASE_URL1 } from "../../../../../api/http-helper";
+import {
+  buildUrl,
+  getHeaders,
+  postHeaders,
+} from "../../../../../api/http-drug-details";
 
 // const GET_DRUG_SUMMARY_LA = BASE_URL1 + "api/1/formulary-drug-summary-la/3303?entity_id=3303";
 
@@ -10,30 +14,14 @@ import { BASE_URL1 } from "../../../../../api/http-helper";
 export const getDrugDetailsLASummary = createAsyncThunk(
   "drug_details/LA_Summary",
   async (apiDetails: any) => {
-    let apiPart = apiDetails.apiPart;
-    let pathParams = apiDetails.pathParams;
-    let keyVals = apiDetails.keyVals;
-    let GET_URL = BASE_URL1 + apiPart + pathParams;
-    if (keyVals) {
-      keyVals = keyVals.map((pair) => pair.key + "=" + pair.value);
-      GET_URL = GET_URL + "?" + keyVals.join("&");
-    }
-    console.log("getTier action creator:: url: " + GET_URL);
-    const requestHeaders = {
-      headers: {
-        Authorization: "Bearer b8625daa-ccbd-4167-8cde-673de141fd11",
-        Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    };
-    console.log("The GET LA SUMMARY url = ", GET_URL);
+    let GET_URL = buildUrl({ apiDetails });
+    const requestHeaders = getHeaders();
     return fetch(GET_URL, requestHeaders)
       .then((response) => {
         if (!response.ok) throw Error(response.statusText);
         return response.json();
       })
       .then((json) => {
-        console.log("Drug DEtails LA Json : ", json);
         return json;
       });
   }
@@ -42,34 +30,14 @@ export const getDrugDetailsLASummary = createAsyncThunk(
 export const getDrugDetailsLAList = createAsyncThunk(
   "drug_details/LA_list",
   async (apiDetails: any) => {
-    let apiPart = apiDetails.apiPart;
-    let pathParams = apiDetails.pathParams;
-    let keyVals = apiDetails.keyVals;
-    let messageBody = apiDetails.messageBody;
-    let POST_URL = BASE_URL1 + apiPart + pathParams;
-    if (keyVals) {
-      keyVals = keyVals.map((pair) => pair.key + "=" + pair.value);
-      POST_URL = POST_URL + "?" + keyVals.join("&");
-    }
-    console.log("postTierApplyInfo action creator:: url: " + POST_URL);
-    const requestHeaders = {
-      method: "POST",
-      body: JSON.stringify(messageBody),
-      headers: {
-        Authorization: "Bearer b8625daa-ccbd-4167-8cde-673de141fd11",
-        Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    };
-    console.log("The LA DRUG LIST Url = ", POST_URL);
-    console.log("The LA DRUG LIST Headers = ", requestHeaders);
+    let POST_URL = buildUrl({ apiDetails });
+    const requestHeaders = postHeaders(apiDetails);
     return fetch(POST_URL, requestHeaders)
       .then((response) => {
         if (!response.ok) throw Error(response.statusText);
         return response.json();
       })
       .then((json) => {
-        console.log("Drug LIST LA Json : ", json);
         return json;
       });
   }
@@ -78,35 +46,15 @@ export const getDrugDetailsLAList = createAsyncThunk(
 export const postReplaceLADrug = createAsyncThunk(
   "drug_details/postReplaceLADrug",
   async (apiDetails: any) => {
-    let apiPart = apiDetails.apiPart;
-    let pathParams = apiDetails.pathParams;
-    let keyVals = apiDetails.keyVals;
-    let messageBody = apiDetails.messageBody;
-    let POST_URL = BASE_URL1 + apiPart + pathParams;
-    if (keyVals) {
-      keyVals = keyVals.map((pair) => pair.key + "=" + pair.value);
-      POST_URL = POST_URL + "?" + keyVals.join("&");
-    }
-    const requestHeaders = {
-      method: "POST",
-      body: JSON.stringify(messageBody),
-      headers: {
-        Authorization: "Bearer b8625daa-ccbd-4167-8cde-673de141fd11",
-        Accept: "application/json",
-        "Content-Type": "application/json;charset=UTF-8",
-      },
-    };
-    console.log("THe Post Url = ", POST_URL);
-    console.log("THe Request Headers = ", requestHeaders);
+    let POST_URL = buildUrl({ apiDetails });
+    const requestHeaders = postHeaders(apiDetails);
     return fetch(POST_URL, requestHeaders)
       .then((response) => {
         if (!response.ok) throw Error(response.statusText);
         return response.json();
       })
       .then((json) => {
-        console.log("postTierApplyInfo: ", json);
         return json;
-      })
-      .catch(error => console.log("THe POST Replace LA Drug ERROR = ", error));
+      });
   }
 );
