72d7d5c6f2d2afaf769995d601d60ffc1f21cba8
anilhegde <ahegde@futurerx.com>
2020-12-23 20:37:36 +0530

    integrating  ql summary and drug detail grid for commercial lob in ql tab

--- src/components/NewFormulary/DrugDetails/components/QL/components/Replace.tsx
+++ src/components/NewFormulary/DrugDetails/components/QL/components/Replace.tsx
@@ -1,35 +1,46 @@
-import React from 'react'
+import React from "react";
 import { Grid } from "@material-ui/core";
 import DropDown from "../../../../../shared/Frx-components/dropdown/DropDown";
 import Label from "../../../../../shared/Frx-components/label/Label";
-import './common.scss';
+import Button from "../../../../../shared/Frx-components/button/Button";
+import "./common.scss";
 
 const Replace = () => {
+  const openTierGridContainer = () => {};
   return (
     <div className="ql-replace-container">
-      <div className="panel-note">By inputting ‘Period of Time in Days’ you are creating a Type 2 QL indicating Quantity Limit over Time. Populate ‘Quantity’ and ‘Days’ to create Type 1 QL indicating Daily Quantity Limit.</div>
+      <div className="panel-note">
+        By inputting ‘Period of Time in Days’ you are creating a Type 2 QL
+        indicating Quantity Limit over Time. Populate ‘Quantity’ and ‘Days’ to
+        create Type 1 QL indicating Daily Quantity Limit.
+      </div>
       <Grid container>
         <Grid item xs={4}>
-            <div className="input-group">      
-              <Label required={true}>QUANTITY</Label> 
-              <DropDown options={[1, 2, 3]} />
-            </div>
-        </Grid>      
+          <div className="input-group">
+            <Label required={true}>QUANTITY</Label>
+            <DropDown options={[1, 2, 3]} />
+          </div>
+        </Grid>
         <Grid item xs={4}>
-            <div className="input-group">      
-              <Label required={true}>DAYS</Label> 
-              <DropDown options={[1, 2, 3]} />
-            </div>
-        </Grid>      
+          <div className="input-group">
+            <Label required={true}>DAYS</Label>
+            <DropDown options={[1, 2, 3]} />
+          </div>
+        </Grid>
         <Grid item xs={4}>
-            <div className="input-group">      
-              <Label required={true}>PERIOD OF TIME IN DAYS</Label> 
-              <DropDown options={[1, 2, 3]} />
-            </div>
-        </Grid>      
-      </Grid>      
+          <div className="input-group">
+            <Label required={true}>PERIOD OF TIME IN DAYS</Label>
+            <DropDown options={[1, 2, 3]} />
+          </div>
+        </Grid>
+        {/* <Button
+          label="Apply"
+          onClick={openTierGridContainer}
+          // disabled={this.props.configureSwitch}
+        ></Button> */}
+      </Grid>
     </div>
-  )
-}
+  );
+};
 
-export default Replace
+export default Replace;
