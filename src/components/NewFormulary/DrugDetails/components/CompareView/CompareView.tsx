a786af9fe26c3f93fe0fd2bc7409117855fc589c
Santosh J Sutar <ssutar@futurerx.com>
2020-12-24 20:55:03 +0530

    Imported all the code related compare/view formularies

--- src/components/NewFormulary/DrugDetails/components/CompareView/CompareView.tsx
+++ src/components/NewFormulary/DrugDetails/components/CompareView/CompareView.tsx
@@ -1,61 +1,111 @@
-import React from 'react';
+import React from "react";
 import { TabInfo } from "../../../../../models/tab.model";
-import FrxTabs from '../../../../shared/FrxTabs/FrxTabs';
-import CompareFormularies from './components/CompareFormularies';
-import ViewFormularies from './components/ViewFormularies';
+import FrxTabs from "../../../../shared/FrxTabs/FrxTabs";
+import CompareFormularies from "./components/CompareFormularies";
+import CompareTable from "./components/CompareAndViewTable/CompareTable";
+import ViewTable from "./components/CompareAndViewTable/ViewTable";
+// import ViewFormularies from "./components/ViewFormularies";
+import { ReactComponent as DownloadIcon } from "../../../../../assets/icons/DownloadIcon.svg";
+import "./CompareView.scss";
+import ViewFormularies from "./components/ViewFormularies";
 
 const tabs = [
-    { id: 1, text: "COMPARE FORMUARIES" },
-    { id: 2, text: "VIEW FORMULARIES" },
-    { id: 3, text: "HPMS SUMMMARY" },
+  { id: 1, text: "COMPARE FORMUARIES" },
+  { id: 2, text: "VIEW FORMULARIES" },
+  { id: 3, text: "HPMS SUMMMARY" },
 ];
 
-interface configureState{
-    tabs: Array<TabInfo>;
-    activeTabIndex: number;
+interface configureState {
+  tabs: Array<TabInfo>;
+  activeTabIndex: number;
+  isCompareClicked: boolean;
+  isViewClicked: boolean;
 }
-interface configureProps{}
+interface configureProps {}
 
-export default class CompareView extends React.Component<configureProps,configureState>{
-    state = {
-        tabs: tabs,
-        activeTabIndex: 0
+export default class CompareView extends React.Component<
+  configureProps,
+  configureState
+> {
+  state = {
+    tabs: tabs,
+    activeTabIndex: 0,
+    isCompareClicked: false,
+    isViewClicked: false,
+  };
+  onClickTab = (selectedTabIndex: number) => {
+    let activeTabIndex = 0;
+
+    const tabs = this.state.tabs.map((tab: TabInfo, index: number) => {
+      if (index === selectedTabIndex) {
+        activeTabIndex = index;
+      }
+      return tab;
+    });
+    this.setState({ tabs, activeTabIndex });
+  };
+
+  handleCompareBtn = () => {
+    this.setState({
+      isCompareClicked: !this.state.isCompareClicked,
+    });
+  };
+
+  handleViewBtn = () => {
+    this.setState({
+      isViewClicked: !this.state.isViewClicked,
+    });
+  };
+  renderActiveTabContent = () => {
+    const tabIndex = this.state.activeTabIndex;
+    switch (tabIndex) {
+      case 0:
+        return <CompareFormularies handleCompareBtn={this.handleCompareBtn} />;
+      case 1:
+        return <ViewFormularies handleViewBtn={this.handleViewBtn} />;
+      case 2:
+        return <div>HPMS SUMMARY</div>;
+      default:
+        return null;
     }
-    onClickTab = (selectedTabIndex: number) => {
-        let activeTabIndex = 0;
-    
-        const tabs = this.state.tabs.map((tab: TabInfo, index: number) => {
-          if (index === selectedTabIndex) {
-            activeTabIndex = index;
-          }
-          return tab;
-        });
-        this.setState({ tabs, activeTabIndex });
-    };
-    renderActiveTabContent = () => {
-        const tabIndex = this.state.activeTabIndex;
-        switch(tabIndex){
-            case 0:
-                return <div><CompareFormularies /> </div>
-            case 1:
-                return <div><ViewFormularies /></div>
-            case 2:
-                return <div>HPMS SUMMARY</div>
-            
-        }
-    }
-    render(){
-        return(
-            <div className="bordered">
-                <FrxTabs
-                    tabList={this.state.tabs}
-                    activeTabIndex={this.state.activeTabIndex}
-                    onClickTab={this.onClickTab}
-                />
-                <div className="inner-container white-bg">
-                    {this.renderActiveTabContent()}
-                </div>
+  };
+  render() {
+    const { activeTabIndex, isCompareClicked, isViewClicked } = this.state;
+    return (
+      <>
+        <div className="bordered">
+          <FrxTabs
+            tabList={this.state.tabs}
+            activeTabIndex={activeTabIndex}
+            onClickTab={this.onClickTab}
+          />
+
+          <div className="inner-container white-bg">
+            {this.renderActiveTabContent()}
+          </div>
+        </div>
+        {activeTabIndex === 0 && isCompareClicked ? (
+          <div className="bordered m-t-10 compare-table-root">
+            <div className="header white-bg flex-container">
+              <label>comparison of formularies</label>
+              <DownloadIcon />
             </div>
-        )
-    }
+            <div className="inner-container white-bg p-10">
+              <CompareTable />
+            </div>
+          </div>
+        ) : null}
+        {activeTabIndex === 1 && isViewClicked ? (
+          <div className="bordered m-t-10 compare-table-root">
+            <div className="header white-bg flex-container">
+              <label>summary of rxcui count</label>
+            </div>
+            <div className="inner-container white-bg p-10">
+              <ViewTable />
+            </div>
+          </div>
+        ) : null}
+      </>
+    );
+  }
 }
--- src/components/NewFormulary/DrugDetails/components/CompareView/components/ViewFormularies.tsx
+++ src/components/NewFormulary/DrugDetails/components/CompareView/components/ViewFormularies.tsx
@@ -1,40 +1,97 @@
-import React from 'react';
+import React from "react";
 import Grid from "@material-ui/core/Grid";
-import Box from '@material-ui/core/Box';
-import Button from '../../../../../shared/Frx-components/button/Button';
-import FormularyComponent from './FormularyComponent'
-import './CompareView.scss';
+import Box from "@material-ui/core/Box";
+import Button from "../../../../../shared/Frx-components/button/Button";
+import { ReactComponent as EditIcon } from "../../../../../../assets/icons/EditIcon.svg";
+import DialogPopup from "../../../../../shared/FrxDialogPopup/FrxDialogPopup";
+// import CloneFormularyPopup from "../../FormularySetUp/components/CloneFormularyPopup";
 
-export default class ViewFormularies extends React.Component<any,any>{
-    render(){
-        return (
-            <div className="compare-formularies-container">
-                <h6>Select formulary to view activity</h6>
-                <div className="view-formulary-form formulay-label">
-                    <Grid container>
-                        <Grid item xs={4}>
-                            <div className="group select-formulary-name">
-                                <label>Formulary Name <span className="astrict">*</span></label>
-                                <input type="text" placeholder="2021Care926-1 v.1" className="base-input" />
-                                <span className="select-formulary-write-icon">
-                                <svg width="17" height="15" viewBox="0 0 17 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M11.6493 2.43847L14.2593 5.08105C14.3692 5.19238 14.3692 5.37402 14.2593 5.48535L7.93981 11.8838L5.25463 12.1855C4.89583 12.2266 4.59201 11.9189 4.63252 11.5557L4.93056 8.83691L11.25 2.43847C11.36 2.32715 11.5394 2.32715 11.6493 2.43847ZM16.3368 1.76758L14.9248 0.33789C14.485 -0.107422 13.7703 -0.107422 13.3275 0.33789L12.3032 1.375C12.1933 1.48633 12.1933 1.66797 12.3032 1.7793L14.9132 4.42187C15.0231 4.5332 15.2025 4.5332 15.3125 4.42187L16.3368 3.38476C16.7766 2.93652 16.7766 2.21289 16.3368 1.76758ZM11.1111 10.1435V13.126H1.85185V3.75097H8.50116C8.59375 3.75097 8.68056 3.71289 8.74711 3.64843L9.90451 2.47656C10.1244 2.2539 9.96817 1.87597 9.65856 1.87597H1.38889C0.622106 1.87597 0 2.50586 0 3.28222V13.5947C0 14.3711 0.622106 15.001 1.38889 15.001H11.5741C12.3409 15.001 12.963 14.3711 12.963 13.5947V8.97167C12.963 8.6582 12.5897 8.50292 12.3698 8.72265L11.2124 9.89452C11.1487 9.9619 11.1111 10.0498 11.1111 10.1435Z" fill="#707683"/>
-                                </svg>
-                                </span>
-                            </div>
-                        </Grid>
-                        <Grid item xs={2}>
-                            <Box display="flex" justifyContent="flex-end" className="view-formulary-btn">
-                                <Button label="View" />
-                            </Box>
-                        </Grid>
+import "./CompareView.scss";
 
-                        <Grid item xs={12}>
-                           <FormularyComponent />
-                        </Grid>
-                    </Grid>
-                </div>
-            </div>
-        )
-    }
+enum PopUpTypes {
+  TYPE1 = "SELECTFORMULARY",
+  TYPE2 = "VIEWFULLFORMULARY",
 }
+class ViewFormularies extends React.Component<any, any> {
+  state = {
+    selectFormulary: false,
+    show: false,
+
+    PopUpType: PopUpTypes.TYPE2,
+
+    hidden: false,
+  };
+  onClose = () => {
+    console.log("close");
+    this.setState({ selectFormulary: false });
+    return true;
+  };
+  handleIconClick = () => {
+    this.setState({ selectFormulary: true });
+  };
+  render() {
+    return (
+      <div className="compare-formularies-container">
+        <h6>Select formulary to view activity</h6>
+        <div className="view-formulary-form formulay-label">
+          <Grid container>
+            <Grid item xs={3}>
+              <div className="group select-formulary-name">
+                <label>
+                  Formulary Name <span className="astrict">*</span>
+                </label>
+
+                <div className="input-element">
+                  <div className="bordered pointer">
+                    <span onClick={(e) => this.handleIconClick()}>
+                      Formulary 1
+                    </span>
+                    <EditIcon
+                      onClick={(e) => this.handleIconClick()}
+                      className={this.state.hidden ? "hide-edit-icon" : ""}
+                    />
+                  </div>
+                </div>
+              </div>
+            </Grid>
+            <Grid item xs={2}>
+              <Box
+                display="flex"
+                justifyContent="center"
+                className="view-formulary-btn"
+              >
+                <Button label="View" onClick={this.props.handleViewBtn} />
+              </Box>
+            </Grid>
+          </Grid>
+        </div>
+        {this.state.selectFormulary ? (
+          <DialogPopup
+            positiveActionText=""
+            negativeActionText="Close"
+            title={
+              this.state.PopUpType === PopUpTypes.TYPE1
+                ? "Select Formulary"
+                : "View Full Formulary"
+            }
+            handleClose={() => {
+              this.setState({
+                selectFormulary: !this.state.selectFormulary,
+              });
+            }}
+            handleAction={() => {}}
+            open={this.state.selectFormulary}
+            showActions={false}
+            className=""
+            height="80%"
+            width="90%"
+          >
+            {/* <SelectFormularyPopUp formularyToggle={this.formularyToggle} /> */}
+            {/* <CloneFormularyPopup /> */}
+          </DialogPopup>
+        ) : null}
+      </div>
+    );
+  }
+}
+export default ViewFormularies;
