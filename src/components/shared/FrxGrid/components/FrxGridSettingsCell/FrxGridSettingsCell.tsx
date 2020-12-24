675860bfc512468453e268647a5ad57e9dacf7a1
Santosh J Sutar <ssutar@futurerx.com>
2020-12-24 21:31:19 +0530

    completed POS tab for drug-details

--- src/components/shared/FrxGrid/components/FrxGridSettingsCell/FrxGridSettingsCell.tsx
+++ src/components/shared/FrxGrid/components/FrxGridSettingsCell/FrxGridSettingsCell.tsx
@@ -23,6 +23,8 @@ export interface FrxGridSettingsCellProps {
   handleCheck?: any;
   settingsMenuItems?: GridMenu[];
   isRowSelectorCheckbox?: boolean;
+  customSettingIcon?: string;
+  className?: string;
 
   handleSettingsComponentMenuClose?: () => void;
   handleMenuClick?: (menuItem: GridMenu) => void;
@@ -102,7 +104,49 @@ class FrxGridSettingsCell extends React.Component<FrxGridSettingsCellProps> {
    */
   renderExpandedCell = () => {
     const isRowSelectionEnabled = this.props.isRowSelectionEnabled;
-
+    let customSettingIcon = this.props.customSettingIcon;
+    if (customSettingIcon) {
+      if (
+        customSettingIcon &&
+        customSettingIcon === "RED-DOT" &&
+        !this.props.dataRow.checked
+      ) {
+        customSettingIcon = "NONE";
+      }
+      switch (customSettingIcon) {
+        case "RED-DOT":
+          return (
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 12 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="6" cy="6" r="6" fill="#E76262" />
+            </svg>
+          );
+        case "PLUS-BTN":
+          return (
+            <svg
+              width="27"
+              height="27"
+              viewBox="0 0 27 27"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.71751 6.71751C3.01352 10.4215 3.01352 16.4485 6.71751 20.1525C10.4215 23.8565 16.4485 23.8565 20.1525 20.1525C23.8565 16.4485 23.8565 10.4215 20.1525 6.71751C16.4485 3.01351 10.4215 3.01352 6.71751 6.71751ZM17.5689 12.7045C17.6664 12.702 17.7634 12.7191 17.8542 12.7546C17.945 12.7902 18.0278 12.8436 18.0977 12.9117C18.1675 12.9798 18.223 13.0612 18.2609 13.1511C18.2988 13.2409 18.3184 13.3375 18.3184 13.435C18.3184 13.5326 18.2988 13.6291 18.2609 13.719C18.223 13.8089 18.1675 13.8902 18.0977 13.9583C18.0278 14.0264 17.945 14.0798 17.8542 14.1154C17.7634 14.151 17.6664 14.1681 17.5689 14.1656L14.1659 14.1659L14.1656 17.5689C14.1607 17.7594 14.0816 17.9405 13.9451 18.0735C13.8086 18.2065 13.6256 18.2809 13.435 18.2809C13.2445 18.2809 13.0614 18.2065 12.925 18.0735C12.7885 17.9405 12.7094 17.7594 12.7045 17.5689L12.7042 14.1659L9.30117 14.1656C9.11066 14.1607 8.9296 14.0816 8.79658 13.9451C8.66356 13.8086 8.58911 13.6256 8.58911 13.435C8.58911 13.2445 8.66356 13.0614 8.79658 12.925C8.9296 12.7885 9.11066 12.7094 9.30117 12.7045L12.7042 12.7042L12.7045 9.30117C12.7094 9.11066 12.7885 8.9296 12.925 8.79658C13.0614 8.66356 13.2445 8.58911 13.435 8.58911C13.6256 8.58911 13.8086 8.66356 13.9451 8.79658C14.0816 8.9296 14.1607 9.11066 14.1656 9.30117L14.1659 12.7042L17.5689 12.7045Z"
+                fill="#1D54B4"
+              />
+            </svg>
+          );
+        case "NONE":
+          return null;
+        default:
+          return null;
+      }
+    }
     return (
       <>
         {!isRowSelectionEnabled ? (
@@ -158,6 +202,49 @@ class FrxGridSettingsCell extends React.Component<FrxGridSettingsCellProps> {
    */
   renderNonExpandedCell = () => {
     const isRowSelectionEnabled = this.props.isRowSelectionEnabled;
+    let customSettingIcon = this.props.customSettingIcon;
+    if (customSettingIcon) {
+      if (
+        customSettingIcon &&
+        customSettingIcon === "RED-DOT" &&
+        !this.props.dataRow.checked
+      ) {
+        customSettingIcon = "NONE";
+      }
+      switch (customSettingIcon) {
+        case "RED-DOT":
+          return (
+            <svg
+              width="12"
+              height="12"
+              viewBox="0 0 12 12"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <circle cx="6" cy="6" r="6" fill="#E76262" />
+            </svg>
+          );
+        case "PLUS-BTN":
+          return (
+            <svg
+              width="27"
+              height="27"
+              viewBox="0 0 27 27"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M6.71751 6.71751C3.01352 10.4215 3.01352 16.4485 6.71751 20.1525C10.4215 23.8565 16.4485 23.8565 20.1525 20.1525C23.8565 16.4485 23.8565 10.4215 20.1525 6.71751C16.4485 3.01351 10.4215 3.01352 6.71751 6.71751ZM17.5689 12.7045C17.6664 12.702 17.7634 12.7191 17.8542 12.7546C17.945 12.7902 18.0278 12.8436 18.0977 12.9117C18.1675 12.9798 18.223 13.0612 18.2609 13.1511C18.2988 13.2409 18.3184 13.3375 18.3184 13.435C18.3184 13.5326 18.2988 13.6291 18.2609 13.719C18.223 13.8089 18.1675 13.8902 18.0977 13.9583C18.0278 14.0264 17.945 14.0798 17.8542 14.1154C17.7634 14.151 17.6664 14.1681 17.5689 14.1656L14.1659 14.1659L14.1656 17.5689C14.1607 17.7594 14.0816 17.9405 13.9451 18.0735C13.8086 18.2065 13.6256 18.2809 13.435 18.2809C13.2445 18.2809 13.0614 18.2065 12.925 18.0735C12.7885 17.9405 12.7094 17.7594 12.7045 17.5689L12.7042 14.1659L9.30117 14.1656C9.11066 14.1607 8.9296 14.0816 8.79658 13.9451C8.66356 13.8086 8.58911 13.6256 8.58911 13.435C8.58911 13.2445 8.66356 13.0614 8.79658 12.925C8.9296 12.7885 9.11066 12.7094 9.30117 12.7045L12.7042 12.7042L12.7045 9.30117C12.7094 9.11066 12.7885 8.9296 12.925 8.79658C13.0614 8.66356 13.2445 8.58911 13.435 8.58911C13.6256 8.58911 13.8086 8.66356 13.9451 8.79658C14.0816 8.9296 14.1607 9.11066 14.1656 9.30117L14.1659 12.7042L17.5689 12.7045Z"
+                fill="#1D54B4"
+              />
+            </svg>
+          );
+        case "NONE":
+          return null;
+        default:
+          return null;
+      }
+    }
     return (
       <>
         {!isRowSelectionEnabled ? (
