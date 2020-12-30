34ebd4aac2b2a087725432f27cbf019ea0b9b59a
Bilal Bhatti <bilal@yobo.dev>
2020-12-30 16:05:55 +0500

    added assembly tab

--- src/components/NewFormulary/DrugDetails/components/FormularyConfigure/FormularyConfigure.tsx
+++ src/components/NewFormulary/DrugDetails/components/FormularyConfigure/FormularyConfigure.tsx
@@ -3,59 +3,61 @@ import { TabInfo } from "../../../../../models/tab.model";
 import FrxTabs from "../../../../shared/FrxTabs/FrxTabs";
 import DrugDetails from "./components/DrugDetails";
 import Tier from "./components/Tier";
-import PaData from './components/PA/PaData';
+import PaData from "./components/PA/PaData";
 import StepTherpayDetails from "./components/StepTherapyData";
 import CategoryClass from "./components/CategoryClass";
-import QL from '../QL/QL';
-import {connect} from 'react-redux';
+import QL from "../QL/QL";
+import Assembly from "./components/Assembly";
+import { connect } from "react-redux";
 import { setAdvancedSearch } from "../../../../../redux/slices/formulary/advancedSearch/advancedSearchSlice";
 
 const tabs = [
-  { id: 1, text: "TIER" },
-  { id: 2, text: "CATEGORY/CLASS" },
-  { id: 3, text: "PA" },
-  { id: 4, text: "ST" },
-  { id: 5, text: "QL" },
-  { id: 6, text: "DRUG DETAILS" },
+  { id: 1, text: "ASSEMBLY" },
+  { id: 2, text: "TIER" },
+  { id: 3, text: "CATEGORY/CLASS" },
+  { id: 4, text: "PA" },
+  { id: 5, text: "ST" },
+  { id: 6, text: "QL" },
+  { id: 7, text: "OTHER UM EDITS" },
 ];
 
 function mapDispatchToProps(dispatch) {
   return {
-    setAdvancedSearch: (a) => dispatch(setAdvancedSearch(a))
+    setAdvancedSearch: (a) => dispatch(setAdvancedSearch(a)),
   };
 }
 
 const mapStateToProps = (state) => {
   //console.log(state)
-  return{
+  return {
     current_formulary: state?.application?.formulary,
     edit_info: state?.application?.formulary?.edit_info,
-  }
-}
+  };
+};
 
 interface configureState {
   tabs: Array<TabInfo>;
   activeTabIndex: number;
 }
 interface configureProps {
-  setAdvancedSearch : (a) => void;
-  edit_info: any[],
-  showDrugDetails: boolean,
+  setAdvancedSearch: (a) => void;
+  edit_info: any[];
+  showDrugDetails: boolean;
 }
 
-class FormularyConfigure extends React.Component<
-  any,
-  any
-> {
+class FormularyConfigure extends React.Component<any, any> {
   state = {
     tabs: tabs,
     activeTabIndex: 0,
     showDrugDetails: true,
   };
   componentDidMount() {
-    if(this.props.edit_info) {
-      for(let i=0; i<this.props.edit_info.length; i++) {
-        if(this.props.edit_info[i]['id_edit'] === 68 && this.props.edit_info[i]['id_checked']) {
+    if (this.props.edit_info) {
+      for (let i = 0; i < this.props.edit_info.length; i++) {
+        if (
+          this.props.edit_info[i]["id_edit"] === 68 &&
+          this.props.edit_info[i]["id_checked"]
+        ) {
           // let ddtabs = tabs.filter(e => e.id === 6);
           // if(ddtabs.length > 0) {
           //   tabs.pop();
@@ -74,7 +76,12 @@ class FormularyConfigure extends React.Component<
       }
       return tab;
     });
-    let payload = { advancedSearchBody: {}, populateGrid: false, closeDialog: false, listItemStatus: {} };
+    let payload = {
+      advancedSearchBody: {},
+      populateGrid: false,
+      closeDialog: false,
+      listItemStatus: {},
+    };
     this.props.setAdvancedSearch(payload);
     this.setState({ tabs, activeTabIndex });
   };
@@ -82,28 +89,18 @@ class FormularyConfigure extends React.Component<
     const tabIndex = this.state.activeTabIndex;
     switch (tabIndex) {
       case 0:
-        return (
-          <div>
-            <Tier />
-          </div>
-        );
+        return <Assembly />;
       case 1:
-        return (
-          <div>
-            <CategoryClass />
-          </div>
-        );
+        return <Tier />;
       case 2:
-        return <PaData />;
+        return <CategoryClass />;
       case 3:
-        return (
-          <div>
-            <StepTherpayDetails />
-          </div>
-        );
+        return <PaData />;
       case 4:
-        return <QL />
+        return <StepTherpayDetails />;
       case 5:
+        return <QL />;
+      case 6:
         return this.state.showDrugDetails ? <DrugDetails /> : null;
     }
   };
@@ -123,4 +120,4 @@ class FormularyConfigure extends React.Component<
   }
 }
 
-export default connect(mapStateToProps,mapDispatchToProps)(FormularyConfigure);
+export default connect(mapStateToProps, mapDispatchToProps)(FormularyConfigure);
---
+++ src/components/NewFormulary/DrugDetails/components/FormularyConfigure/components/Assembly/index.tsx
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Assembly extends Component {
+  render() {
+    return <h1>Assembly Component</h1>;
+  }
+}
+
+export default Assembly;
