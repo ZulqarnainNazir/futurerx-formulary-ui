3776dcdf2d3a8ccbfdac8a2d9491b1b686c98354
Ali Ahmad <aliahmad5015@gmail.com>
2021-01-03 14:02:17 +0500

    Radio buttons fixed

--- src/components/NewFormulary/DrugDetails/components/FormularyConfigure/components/PAGroupHeader.tsx
+++ src/components/NewFormulary/DrugDetails/components/FormularyConfigure/components/PAGroupHeader.tsx
@@ -47,20 +47,15 @@ function mapDispatchToProps(dispatch) {
     getPaGrouptDescriptions: (arg) => dispatch(getPaGrouptDescriptions(arg)), // Group List
     getPaTypes: (arg) => dispatch(getPaTypes(arg)), // File Type
     cleanMessage: (arg) => dispatch(cleanMessage(arg)),
-    getPaGrouptDescriptionVersions: (arg) =>
-      dispatch(getPaGrouptDescriptionVersions(arg)), //Version
-    getPaGrouptDescriptionDetail: (arg) =>
-      dispatch(getPaGrouptDescriptionDetail(arg)), // Group ID Detail
+    getPaGrouptDescriptionVersions: (arg) => dispatch(getPaGrouptDescriptionVersions(arg)), //Version
+    getPaGrouptDescriptionDetail: (arg) => dispatch(getPaGrouptDescriptionDetail(arg)), // Group ID Detail
     getPaGrouptDescription: (a) => dispatch(getPaGrouptDescription(a)),
     deleteGroupDescription: (arg) => dispatch(deleteGroupDescription(arg)), // Delete
     cloneGroupDescription: (arg) => dispatch(cloneGroupDescription(arg)), // Clone
     archiveGroupDescription: (arg) => dispatch(archiveGroupDescription(arg)), // archive
-    newVersionGroupDescription: (arg) =>
-      dispatch(newVersionGroupDescription(arg)), // New Vesrion
-    postPAGroupDescriptionFormularies: (arg) =>
-      dispatch(postPAGroupDescriptionFormularies(arg)), // New Vesrion
-    postApplyPAGroupDescriptionFormularies: (arg) =>
-      dispatch(postApplyPAGroupDescriptionFormularies(arg)), // New Vesrion
+    newVersionGroupDescription: (arg) => dispatch(newVersionGroupDescription(arg)), // New Vesrion
+    postPAGroupDescriptionFormularies: (arg) => dispatch(postPAGroupDescriptionFormularies(arg)), // New Vesrion
+    postApplyPAGroupDescriptionFormularies: (arg) => dispatch(postApplyPAGroupDescriptionFormularies(arg)), // New Vesrion
   };
 }
 
@@ -142,9 +137,7 @@ function PAGroupHeader(props: any) {
       setVersion(props.version);
       //const latestVerion = verLength > 0 ? selectedVersion.split(" ")[1] : '';
       setSelectedVersion(props.version[verLength - 1].version_number);
-      setSelectedVersionId(
-        props.version[verLength - 1]["id_pa_group_description"]
-      );
+      setSelectedVersionId(props.version[verLength - 1]["id_pa_group_description"]);
       setPlaceHolder(value);
     } else {
       setVersion([{ value: "Version 1" }]);
@@ -182,15 +175,12 @@ function PAGroupHeader(props: any) {
         isEditable = is_setup.is_setup_complete;
         latestVerion = verLength > 0 ? is_setup.id_pa_group_description : 0;
       } else {
-        isEditable = props.version.find(
-          (val) => val.version_number == Number(selectedVersion.split(" ")[1])
-        ).is_setup_complete;
+        isEditable = props.version.find((val) => val.version_number == Number(selectedVersion.split(" ")[1]))
+          .is_setup_complete;
         latestVerion =
           verLength > 0
-            ? props.version.find(
-                (val) =>
-                  val.version_number == Number(selectedVersion.split(" ")[1])
-              ).id_pa_group_description
+            ? props.version.find((val) => val.version_number == Number(selectedVersion.split(" ")[1]))
+                .id_pa_group_description
             : 0;
       }
 
@@ -249,8 +239,7 @@ function PAGroupHeader(props: any) {
     apiDetails["messageBody"]["effective_date"] = effectiveDate;
     apiDetails["messageBody"]["formulary_ids"] = selectedFormularies;
 
-    apiDetails["messageBody"]["id_pa_group_description"] =
-      props.saveGdm.current_group_des_id;
+    apiDetails["messageBody"]["id_pa_group_description"] = props.saveGdm.current_group_des_id;
     apiDetails["messageBody"]["is_select_all"] = false;
     apiDetails["messageBody"]["pa_group_description_formulary_ids"] = [];
 
@@ -269,11 +258,9 @@ function PAGroupHeader(props: any) {
     if (param === "delete-version") {
       pathParams = selectedVersionId + "/CV?entity_id=" + props.formulary_id;
     } else if (param === "delete-full") {
-      pathParams =
-        props.saveGdm.current_group_id + "/GD?entity_id=" + props.formulary_id;
+      pathParams = props.saveGdm.current_group_id + "/GD?entity_id=" + props.formulary_id;
     } else {
-      pathParams =
-        props.saveGdm.current_group_id + "/GD?entity_id=" + props.formulary_id;
+      pathParams = props.saveGdm.current_group_id + "/GD?entity_id=" + props.formulary_id;
     }
     props
       .deleteGroupDescription({
@@ -283,24 +270,21 @@ function PAGroupHeader(props: any) {
       .then((json) => {
         let apiDetails = {};
         apiDetails["lob_type"] = props.formulary_lob_id;
-        apiDetails["pathParams"] =
-          "/" + props.client_id + "?entity_id=" + props?.formulary_id;
+        apiDetails["pathParams"] = "/" + props.client_id + "?entity_id=" + props?.formulary_id;
         props.getPaGrouptDescriptions(apiDetails).then((json) => {
           const groupList = json?.payload?.data;
           const groupListLength = Object.keys(groupList).length;
           //const id_pa_group_description = groupListLength>0?groupList[0].id_base_pa_group_description:0;
           const id_pa_group_description =
             groupListLength > 0
-              ? groupList.filter((val) => val.is_archived === false)[0]
-                  .id_base_pa_group_description
+              ? groupList.filter((val) => val.is_archived === false)[0].id_base_pa_group_description
               : 0;
           apiDetails["pathParams"] = "/" + id_pa_group_description;
           props.getPaGrouptDescriptionVersions(apiDetails).then((json) => {
             const response = json.payload.data;
             const verLength = Object.keys(response).length;
             const isEditable = response[verLength - 1].is_setup_complete;
-            const latestVerion =
-              response[verLength - 1].id_pa_group_description;
+            const latestVerion = response[verLength - 1].id_pa_group_description;
             const value = response[verLength - 1].value;
             setPanelColor(isEditable ? "-green" : "");
             setVersion(response);
@@ -315,26 +299,16 @@ function PAGroupHeader(props: any) {
         });
 
         if (json?.payload?.status && json?.payload?.status != 200) {
-          if (
-            json?.payload?.data?.formularies &&
-            json?.payload?.data?.formularies?.length > 0
-          ) {
+          if (json?.payload?.data?.formularies && json?.payload?.data?.formularies?.length > 0) {
             let errs = "";
             json.payload.data.formularies.map((val) => {
               errs += "\n\n" + val.formuary_name + "\n\n";
             });
-            showMessage(
-              "Following Formularies are linked to current Group Description:\n" +
-                errs,
-              "error"
-            );
+            showMessage("Following Formularies are linked to current Group Description:\n" + errs, "error");
           }
           showMessage(json.payload.data.message, "error");
         }
-        if (
-          json?.payload?.success?.status &&
-          json?.payload?.success?.status == 200
-        ) {
+        if (json?.payload?.success?.status && json?.payload?.success?.status == 200) {
           showMessage(SUCCESS_MSG["delete"], "success");
         }
         setOpen(false);
@@ -355,10 +329,7 @@ function PAGroupHeader(props: any) {
         if (json?.payload?.status && json?.payload?.status != 200) {
           showMessage(json.payload.data.message, "error");
         }
-        if (
-          json?.payload?.success?.status &&
-          json?.payload?.success?.status == 200
-        ) {
+        if (json?.payload?.success?.status && json?.payload?.success?.status == 200) {
           showMessage(SUCCESS_MSG["clone"], "success");
         }
         setOpen(false);
@@ -369,11 +340,9 @@ function PAGroupHeader(props: any) {
     if (param === "archive-version") {
       pathParams = selectedVersionId + "/CV?entity_id=" + props.formulary_id;
     } else if (param === "archive-full") {
-      pathParams =
-        props.saveGdm.current_group_id + "/GD?entity_id=" + props.formulary_id;
+      pathParams = props.saveGdm.current_group_id + "/GD?entity_id=" + props.formulary_id;
     } else {
-      pathParams =
-        props.saveGdm.current_group_id + "/GD?entity_id=" + props.formulary_id;
+      pathParams = props.saveGdm.current_group_id + "/GD?entity_id=" + props.formulary_id;
     }
     props
       .archiveGroupDescription({
@@ -400,10 +369,7 @@ function PAGroupHeader(props: any) {
         if (json?.payload?.status && json?.payload?.status != 200) {
           showMessage(json.payload.data.message, "error");
         }
-        if (
-          json?.payload?.success?.status &&
-          json?.payload?.success?.status == 200
-        ) {
+        if (json?.payload?.success?.status && json?.payload?.success?.status == 200) {
           showMessage(SUCCESS_MSG["newVersion"], "success");
         }
         setOpen(false);
@@ -444,38 +410,29 @@ function PAGroupHeader(props: any) {
         });
 
         if (json?.payload?.status && json?.payload?.status != 200) {
-          if (
-            json?.payload?.data?.formularies &&
-            json?.payload?.data?.formularies?.length > 0
-          ) {
+          if (json?.payload?.data?.formularies && json?.payload?.data?.formularies?.length > 0) {
             let errs = "";
             json.payload.data.formularies.map((val) => {
               errs += "\n\n" + val.formuary_name + "\n\n";
             });
-            showMessage(
-              "Following Formularies are linked to current Group Description:\n" +
-                errs,
-              "error"
-            );
+            showMessage("Following Formularies are linked to current Group Description:\n" + errs, "error");
           }
           showMessage(json.payload.data.message, "error");
         }
-        if (
-          json?.payload?.success?.status &&
-          json?.payload?.success?.status == 200
-        ) {
+        if (json?.payload?.success?.status && json?.payload?.success?.status == 200) {
           showMessage(SUCCESS_MSG["archive"], "success");
         }
         setOpen(false);
       });
   };
   return (
-    <div className={`version-wrapper${panelColor}`}>
+    <div className={`version-wrapper version-wrapper${panelColor}`}>
       <select
         name="group-description"
         id="group-description"
         className="formulary-type-dropdown formulary-versions"
         onChange={onChange}
+        // style={{ background: "transparent !important", border: "none" }}
       >
         <option value=""></option>
         {versionList.map((e, index) =>
@@ -489,13 +446,7 @@ function PAGroupHeader(props: any) {
         )}
       </select>
       <div className="item" onClick={toggleShowViewAll}>
-        <svg
-          width="11"
-          height="11"
-          viewBox="0 0 11 11"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M10.8281 5.48992C10.8336 8.42555 8.43949 10.826 5.50387 10.8281C4.23597 10.829 3.07134 10.387 2.15613 9.64838C1.91815 9.4563 1.90036 9.09964 2.11662 8.88338L2.35868 8.64132C2.54364 8.45636 2.83892 8.43612 3.04384 8.59869C3.71813 9.13376 4.57147 9.45312 5.5 9.45312C7.68507 9.45312 9.45312 7.68472 9.45312 5.5C9.45312 3.31493 7.68472 1.54688 5.5 1.54688C4.45126 1.54688 3.49875 1.95441 2.79151 2.61963L3.88193 3.71005C4.09849 3.92661 3.94511 4.29688 3.63887 4.29688H0.515625C0.325768 4.29688 0.171875 4.14298 0.171875 3.95312V0.829877C0.171875 0.523639 0.542137 0.370262 0.758699 0.586803L1.81943 1.64753C2.77597 0.733391 4.07241 0.171875 5.5 0.171875C8.43928 0.171875 10.8227 2.55191 10.8281 5.48992ZM6.94134 7.18255L7.15238 6.9112C7.32722 6.68641 7.28673 6.36245 7.06193 6.18763L6.1875 5.5075V3.26562C6.1875 2.98085 5.95665 2.75 5.67187 2.75H5.32812C5.04335 2.75 4.8125 2.98085 4.8125 3.26562V6.18L6.21777 7.273C6.44256 7.44782 6.7665 7.40734 6.94134 7.18255Z"
             fill="#F65A1C"
@@ -504,13 +455,7 @@ function PAGroupHeader(props: any) {
         Version History
       </div>
       <div className="item" onClick={() => handleClickOpen("clone")}>
-        <svg
-          width="13"
-          height="13"
-          viewBox="0 0 13 13"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg width="13" height="13" viewBox="0 0 13 13" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M11.7812 0C12.4544 0 13 0.545645 13 1.21875V8.53125C13 9.20436 12.4544 9.75 11.7812 9.75H4.46875C3.79564 9.75 3.25 9.20436 3.25 8.53125V1.21875C3.25 0.545645 3.79564 0 4.46875 0H11.7812ZM4.46875 10.5625C3.34872 10.5625 2.4375 9.65128 2.4375 8.53125V3.25H1.21875C0.545645 3.25 0 3.79564 0 4.46875V11.7812C0 12.4544 0.545645 13 1.21875 13H8.53125C9.20436 13 9.75 12.4544 9.75 11.7812V10.5625H4.46875Z"
             fill="#F65A1C"
@@ -519,13 +464,7 @@ function PAGroupHeader(props: any) {
         Clone
       </div>
       <div className="item" onClick={() => handleClickOpen("newVersion")}>
-        <svg
-          width="13"
-          height="12"
-          viewBox="0 0 13 12"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg width="13" height="12" viewBox="0 0 13 12" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M6.61523 0C3.47441 0 0.927734 2.54668 0.927734 5.6875C0.927734 8.82832 3.47441 11.375 6.61523 11.375C9.75605 11.375 12.3027 8.82832 12.3027 5.6875C12.3027 2.54668 9.75605 0 6.61523 0ZM9.05273 5.99219C9.05273 6.04805 9.00703 6.09375 8.95117 6.09375H7.02148V8.02344C7.02148 8.0793 6.97578 8.125 6.91992 8.125H6.31055C6.25469 8.125 6.20898 8.0793 6.20898 8.02344V6.09375H4.2793C4.22344 6.09375 4.17773 6.04805 4.17773 5.99219V5.38281C4.17773 5.32695 4.22344 5.28125 4.2793 5.28125H6.20898V3.35156C6.20898 3.2957 6.25469 3.25 6.31055 3.25H6.91992C6.97578 3.25 7.02148 3.2957 7.02148 3.35156V5.28125H8.95117C9.00703 5.28125 9.05273 5.32695 9.05273 5.38281V5.99219Z"
             fill="#F65A1C"
@@ -534,13 +473,7 @@ function PAGroupHeader(props: any) {
         New Version
       </div>
       <div className="item" onClick={() => handleClickOpen("delete")}>
-        <svg
-          width="11"
-          height="11"
-          viewBox="0 0 11 11"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             fill-rule="evenodd"
             clip-rule="evenodd"
@@ -551,13 +484,7 @@ function PAGroupHeader(props: any) {
         Delete
       </div>
       <div className="item" onClick={() => handleClickOpen("archive")}>
-        <svg
-          width="11"
-          height="11"
-          viewBox="0 0 11 11"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
+        <svg width="11" height="11" viewBox="0 0 11 11" fill="none" xmlns="http://www.w3.org/2000/svg">
           <path
             d="M0.6875 9.625C0.6875 10.0053 0.994727 10.3125 1.375 10.3125H9.625C10.0053 10.3125 10.3125 10.0053 10.3125 9.625V3.4375H0.6875V9.625ZM4.125 5.07031C4.125 4.92852 4.24102 4.8125 4.38281 4.8125H6.61719C6.75898 4.8125 6.875 4.92852 6.875 5.07031V5.24219C6.875 5.38398 6.75898 5.5 6.61719 5.5H4.38281C4.24102 5.5 4.125 5.38398 4.125 5.24219V5.07031ZM10.3125 0.6875H0.6875C0.307227 0.6875 0 0.994727 0 1.375V2.40625C0 2.59531 0.154687 2.75 0.34375 2.75H10.6562C10.8453 2.75 11 2.59531 11 2.40625V1.375C11 0.994727 10.6928 0.6875 10.3125 0.6875Z"
             fill="#F65A1C"
@@ -566,12 +493,7 @@ function PAGroupHeader(props: any) {
         Archive
       </div>
       {open ? (
-        <STAlertDialog
-          popuptitle={props.popuptitle}
-          openPopup={open}
-          closePopup={handleClose}
-          popupType={popupType}
-        >
+        <STAlertDialog popuptitle={props.popuptitle} openPopup={open} closePopup={handleClose} popupType={popupType}>
           <Alerts
             closePopup={handleClose}
             popupType={popupType}
