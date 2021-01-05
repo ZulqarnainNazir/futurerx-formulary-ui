463530e8c2316be0540891c211fdae0f51c79172
hanehosur <hanehosur@futurerx.com>
2021-01-05 11:46:43 +0530

    Added fix to checkbox not checking on compre/view

--- src/components/NewFormulary/DrugDetails/components/CompareView/components/CompareAndViewTable/CompareTable.tsx
+++ src/components/NewFormulary/DrugDetails/components/CompareView/components/CompareAndViewTable/CompareTable.tsx
@@ -2079,6 +2079,7 @@ class CompareTable extends Component<any, any> {
             height="80%"
             width="80%"
             open={showViewAll}
+            className="dialog-popup clone-dialog-popup"
           >
             <FrxGridContainer
               enableSearch={false}
