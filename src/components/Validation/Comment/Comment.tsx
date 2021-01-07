7a514179cce2ec673cdd16a73c1c5ac822e675e2
DhirajPanchal <dpanchal@futurerx.com>
2021-01-07 15:01:23 +0530

    Formatting

--- src/components/Validation/Comment/Comment.tsx
+++ src/components/Validation/Comment/Comment.tsx
@@ -1,42 +1,42 @@
-import React from 'react'
-import {Row, Col, Avatar,Space} from "antd";
+import React from "react";
+import { Row, Col, Avatar, Space } from "antd";
 import "./Comment.css";
 
-interface Props{
-  element:any
+interface Props {
+  element: any;
 }
 
-function Comment(props:Props) {
-  const {validation_name,notes_count,display_date} = props.element
+function Comment(props: Props) {
+  const { validation_name, notes_count, display_date } = props.element;
   return (
     <Row className="Comment-card" align="middle">
       <Col xs={24} lg={12}>
         <Space size="large">
-        <Avatar style={{ backgroundColor: "#EE5959", verticalAlign: 'middle' }}>
-        {1}
-      </Avatar>
-      <span className="title">{validation_name}</span>
+          <Avatar
+            style={{ backgroundColor: "#EE5959", verticalAlign: "middle" }}
+          >
+            {1}
+          </Avatar>
+          <span className="title">{validation_name}</span>
         </Space>
       </Col>
-      <Col xs={24} lg={4} style={{textAlign:"center"}}>
+      <Col xs={24} lg={4} style={{ textAlign: "center" }}>
         <Space size="large">
-          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"/>
+          <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
           <span className="date">{display_date}</span>
         </Space>
       </Col>
-      <Col xs={24} lg={4} style={{textAlign:"center"}}>
+      <Col xs={24} lg={4} style={{ textAlign: "center" }}>
         <Space size="small">
           {notes_count}
-          <img src="/images/comment.png" alt=""/>
+          <img src="/images/comment.png" alt="" />
         </Space>
       </Col>
-      <Col xs={24} lg={4} style={{textAlign:"right"}}>
-
-          <img src="/images/arrow.png" alt=""/>
-  
+      <Col xs={24} lg={4} style={{ textAlign: "right" }}>
+        <img src="/images/arrow.png" alt="" />
       </Col>
     </Row>
-  )
+  );
 }
 
-export default Comment
+export default Comment;
