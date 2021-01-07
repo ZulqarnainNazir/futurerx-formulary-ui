e3ed56db7aca262e700b6f5cce0d746e0ba4f31a
arsalanrasheed <arsalan.rasheed93@gmail.com>
2021-01-07 23:03:14 +0500

    feature/ui-work

--- src/mocks/formulary/dashboardStats.ts
+++ src/mocks/formulary/dashboardStats.ts
@@ -1,23 +1,73 @@
-export const getStats = () => {
+export const getMedicareStats = () => {
     return [
         {
-            label: 'Selling',
-            value: "30"
+            label: 'Sell',
+            value: "85"
         },
         {
-            label: 'Created',
-            value: "15"
+            label: 'Create',
+            value: "10"
         },
         {
-            label: 'Purchased',
-            value: "1"
+            label: 'Purchase',
+            value: "3"
+        },
+    ]
+}
+
+export const getMediCaidStats = () => {
+    return [
+        {
+            label: 'Sell',
+            value: "38"
+        },
+        {
+            label: 'Create',
+            value: "4"
+        },
+        {
+            label: 'Purchase',
+            value: "0"
+        },
+    ]
+}
+export const getCommercialStats = () => {
+    return [
+        {
+            label: 'Sell',
+            value: "111"
+        },
+        {
+            label: 'Create',
+            value: "21"
+        },
+        {
+            label: 'Purchase',
+            value: "5"
+        },
+    ]
+}
+
+export const getExchangeStats = () => {
+    return [
+        {
+            label: 'Sell',
+            value: "29"
+        },
+        {
+            label: 'Create',
+            value: "0"
+        },
+        {
+            label: 'Purchase',
+            value: "2"
         },
     ]
 }
 
 export const getStatsChart = () => {
     return {
-        average: 38,
+        average: 335,
         value1: 30,
         value2: 60,
         value3: 10,
