c718f781c79a6c31c0fef2500ab189159ce78aae
DhirajPanchal <dpanchal@futurerx.com>
2021-01-13 14:32:35 +0530

    Validation now points Common Header obj.

--- src/redux/slices/formulary/validation/validationActionCreation.ts
+++ src/redux/slices/formulary/validation/validationActionCreation.ts
@@ -1,18 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import { BASE_URL1 } from "../../../../api/http-helper";
+import { REQUEST_HEADER } from "../../../../api/http-commons";
 
 const URL = BASE_URL1 + "/api/1/formulary-validations/";
 export const getValidationList = createAsyncThunk(
   "formulary/getValidationList",
   async (formulary_id: string) => {
-    const requestHeaders  = {
-        headers: {
-            'Authorization': 'Bearer 1e05ff8b-a0af-4a8f-8915-487321900f21',
-            'Accept': 'application/json',
-            'Content-Type': 'application/json;charset=UTF-8',
-        }
-    }
-    return fetch(URL + formulary_id,requestHeaders)
+    const requestHeaders = {
+      // headers: REQUEST_HEADER,
+    };
+    return fetch(URL + formulary_id, requestHeaders)
       .then((response) => {
         if (!response.ok) throw Error(response.statusText);
         return response.json();
