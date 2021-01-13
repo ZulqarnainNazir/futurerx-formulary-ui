8ea9fbbe677565d634a1802e1e83b3146a33da0b
arsalanrasheed <arsalan.rasheed93@gmail.com>
2021-01-14 01:51:17 +0500

    workflow popop 80% done

--- src/components/navigation-bar/components/CommonDialogActions/components/Workflow-Dialog/WorkFlow-Dialog.tsx
+++ src/components/navigation-bar/components/CommonDialogActions/components/Workflow-Dialog/WorkFlow-Dialog.tsx
@@ -1,15 +1,27 @@
 import React from "react";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-import ExpandLessIcon from "@material-ui/icons/ExpandLess";
 import './WorkFlow-Dialog.scss';
-import { Grid } from "@material-ui/core";
-import DropDownMap from "../../../../../shared/Frx-components/dropdown/DropDownMap";
 import DropDown from "../../../../../shared/Frx-components/dropdown/DropDownMap";
 import FrxTimeProgressBar from "../../../../../shared/FrxTimeProgressBar/FrxTimeProgressBar";
-import {ReactComponent as OrangeDot} from "../../../../../../assets/icons/orange-dot.svg";
+import { ReactComponent as OrangeDot } from "../../../../../../assets/icons/orange-dot.svg";
+
+
+const EyeIcon = (props) => (
+  <span {...props}>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="16"
+      height="12"
+      viewBox="0 0 16 12"
+      fill="none"
+    >
+      <path
+        d="M15.6548 5.60078C14.172 2.70754 11.2364 0.75 7.87498 0.75C4.51362 0.75 1.57717 2.70891 0.0951413 5.60105C0.0325903 5.72479 0 5.86149 0 6.00014C0 6.13878 0.0325903 6.27549 0.0951413 6.39922C1.57799 9.29246 4.51362 11.25 7.87498 11.25C11.2364 11.25 14.1728 9.29109 15.6548 6.39895C15.7174 6.27521 15.75 6.13851 15.75 5.99986C15.75 5.86122 15.7174 5.72451 15.6548 5.60078ZM7.87498 9.9375C7.09622 9.9375 6.33495 9.70657 5.68743 9.27391C5.03991 8.84125 4.53523 8.2263 4.23721 7.50682C3.93919 6.78733 3.86121 5.99563 4.01314 5.23183C4.16507 4.46803 4.54008 3.76644 5.09075 3.21577C5.64142 2.6651 6.34302 2.29009 7.10682 2.13816C7.87062 1.98623 8.66232 2.0642 9.3818 2.36222C10.1013 2.66024 10.7162 3.16492 11.1489 3.81244C11.5816 4.45996 11.8125 5.22124 11.8125 6C11.8127 6.51715 11.7111 7.02928 11.5133 7.50711C11.3155 7.98495 11.0255 8.41911 10.6598 8.78479C10.2941 9.15047 9.85993 9.4405 9.3821 9.63829C8.90427 9.83608 8.39213 9.93775 7.87498 9.9375ZM7.87498 3.375C7.64068 3.37827 7.4079 3.41313 7.18291 3.47863C7.36836 3.73065 7.45736 4.04079 7.43376 4.3528C7.41015 4.66481 7.27551 4.95802 7.05426 5.17928C6.83301 5.40053 6.53979 5.53517 6.22778 5.55877C5.91577 5.58237 5.60564 5.49338 5.35362 5.30793C5.21011 5.83665 5.23601 6.39705 5.42769 6.91027C5.61936 7.42349 5.96715 7.86369 6.4221 8.16889C6.87706 8.4741 7.41627 8.62895 7.96384 8.61166C8.51141 8.59436 9.03978 8.40578 9.47456 8.07247C9.90934 7.73915 10.2287 7.27789 10.3876 6.75359C10.5465 6.22929 10.5369 5.66837 10.3603 5.14977C10.1837 4.63116 9.84897 4.18099 9.40313 3.86262C8.95728 3.54425 8.42283 3.37371 7.87498 3.375Z"
+        fill="#707683"
+      />
+    </svg>
+  </span>
+);
+
 export default class WorkFlowDialog extends React.Component<any, any> {
   state = {
     expandCloseIconInd: true,
@@ -94,7 +106,37 @@ export default class WorkFlowDialog extends React.Component<any, any> {
         {
           !this.state.expandCloseIconInd &&
           <div className="expandedDetailWrapper">
-            <div className="expandedLeftSection">
+            <div className="expandedLeftSection">             
+            <div className="row">
+                <span className="statusIcon"></span>
+                <label>Parent task name</label>
+                <div className="taskPicWrapper">
+                  <span>
+
+                  </span>
+                </div>
+                <span className="eyeIcon">
+                  <EyeIcon />
+                </span>
+              </div>
+
+              <div className="row">
+                <span className="statusIcon"></span>
+                <p style={{color:"#707683"}}>SUBTASKS</p>                
+              </div>
+
+              <div className="row">
+                <span className="statusIcon"><OrangeDot /></span>
+                <p style={{color:"#333333"}}>Subtask number 1</p>
+                <div className="taskPicWrapper">
+                  <span>
+
+                  </span>
+                </div>
+                <span className="eyeIcon">
+                  <EyeIcon />
+                </span>
+              </div>
 
             </div>
             <div className="expandedMiddleSection">
@@ -107,10 +149,15 @@ export default class WorkFlowDialog extends React.Component<any, any> {
                 </div>
                 <div className="workFlowNoteDetail">
                   <div className="workFlowNoteDetailPic">
+                      <span>
 
+                      </span>
                   </div>
                   <div className="workFlowNoteDetailText">
-
+                    <label>Kristine Watson</label>
+                    <span>2 hours ago</span>
+                    <p>We need to figure out what's going on with the last three items</p>
+                    <div>Version: 2</div>
                   </div>
                 </div>
               </div>
