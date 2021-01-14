dc318b5cf9847a6785d5672f01cde525526f4154
Mubeen Rana <rmubeen35@gmail.com>
2021-01-14 16:17:45 +0500

    view popup

--- src/components/navigation-bar/components/CommonDialogActions/components/Audit.tsx
+++ src/components/navigation-bar/components/CommonDialogActions/components/Audit.tsx
@@ -1,9 +1,60 @@
 import React from "react";
+import FrxGridContainer from "../../../../shared/FrxGrid/FrxGridContainer";
+import { MemberAuditViewColumns } from "../../../../../utils/grid/columns";
+import { getAuditViewData } from "../../../../../mocks/grid/audit-view-mock";
 
-export default class WorkFlow extends React.Component<any, any> {  
+
+export default class MemberAudit extends React.Component<any, any> {  
+  state = {
+    isFetchingData: false,
+    data: [] as any[],
+    filteredData: [] as any[]
+  };
+
+  componentDidMount() {
+    //fetch data from API
+    const data = getAuditViewData();
+    this.setState({ data, filteredData: data });
+  }
+
+  handleSearch = searchObject => {
+    console.log(searchObject);
+    this.setState({ isFetchingData: true });
+    if (searchObject && searchObject.status) {
+      setTimeout(() => {
+        const newData = this.state.data.filter(
+          d => d.status === searchObject.status
+        );
+        this.setState({ isFetchingData: false, filteredData: newData });
+      }, 2000);
+    } else {
+      this.setState({ isFetchingData: false });
+    }
+  };
+
+
   render() {
+    const columns = MemberAuditViewColumns();
     return (
-        <div>Audit POPUP</div>
+        <FrxGridContainer
+					enableSearch
+					enableSettings
+          onSearch={this.handleSearch}
+          fixedColumnKeys={[""]}
+          gridName="AuditView"
+          isFetchingData={this.state.isFetchingData}
+          columns={columns}
+          data={this.state.filteredData}
+          pagintionPosition="topRight"
+          onSettingsClick="grid-menu"
+          applyFilter
+          customSettingIcon={" "}
+          scroll={{ x: 1100, y: 420 }}
+          expandable={{
+            isExpandable: false,
+           
+          }}
+        />
     );
   }
 }
---
+++ src/mocks/grid/audit-view-mock.ts
@@ -0,0 +1,39 @@
+export const getAuditViewData = () => {
+  return [
+    {
+      id: 1,
+      key: 1,
+      date: "23/1/2021",
+      start : "11:04:11 AM ",
+      end : "12:04:11 PM ",
+      subMenu:"Simulations",
+      newValue:"Test",
+      task:"View Simuations",
+      user:"Annet Black"
+    },
+      {
+      id: 2,
+      key: 2,
+      date: "23/1/2021",
+      start : "11:04:11 AM ",
+      end : "12:04:11 PM ",
+      subMenu:"Simulations",
+      newValue:"Test",
+      task:"View Formulary Compliance",
+      user:"Jhon Robertson"
+    },
+     {
+      id: 3,
+      key: 3,
+      date: "23/1/2021",
+      start : "11:04:11 AM ",
+      end : "12:04:11 PM ",
+      subMenu:"Simulations",
+      newValue:"Test",
+      task:"View Simuations",
+      user:"Lesile Alaxander"
+    },
+   
+   
+  ];
+};
